40ba68a051396c2f1d950753fb925109a9f1b489
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2025-02-07 23:14:18 +0200

    Update header html

--- includes/header.php
+++ includes/header.php
@@ -1,17 +1,17 @@
 <header class="header">
     <div class="header__box template__inherit template--content">
-        <div class="header__content">
+        <div class="header__content flex flex-space-btw">
             <div class="header__logo logo">
                 <a href="#" class="logo__link">My portfolio</a>
             </div>
-            <nav class="navigation">
-                <ul class="menu">
+            <nav class="navigation flex flex-align-center">
+                <ul class="menu flex">
                     <li class="menu__item">
                         <a href="#" class="menu__link">Menu-category-1</a>
                     </li>
                 </ul>
             </nav>
-            <div class="accout">
+            <div class="accout flex flex-align-center">
                 <ul class="accout__meun">
                     <li class="account__item">
                         <a href="#" class="account__link">My account</a>
