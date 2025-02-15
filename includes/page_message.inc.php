5c45c9506bbfafa92164fe46fd0f289590ecafcb
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2025-02-15 09:45:56 +0200

    Add page message layout

---
+++ includes/page_message.inc.php
@@ -0,0 +1,5 @@
+<div class="page__message message template--modal">
+    <div class="message__body">
+        <span class="message__text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</span>
+    </div>
+</div>
