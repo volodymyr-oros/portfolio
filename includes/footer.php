44341658cf0be99c785abc02f8440689d011f53a
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2025-02-18 16:55:23 +0200

    Update footer layout

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,38 @@
 <footer class="footer template__subgrid">
     <div class="footer__content template__width-content">
-        <p>Footer</p>
+        <div class="footer__row">
+            <ul class="footer__column">
+                <li class="footer__item">
+                    <a href="#" class="footer_link">somelink</a>
+                    <a href="#" class="footer_link">somelink</a>
+                    <a href="#" class="footer_link">somelink</a>
+                    <a href="#" class="footer_link">somelink</a>
+                    <a href="#" class="footer_link">somelink</a>
+                </li>
+            </ul>
+            <ul class="footer__column">
+                <li class="footer__item">
+                    <a href="#" class="footer_link">somelink</a>
+                    <a href="#" class="footer_link">somelink</a>
+                    <a href="#" class="footer_link">somelink</a>
+                    <a href="#" class="footer_link">somelink</a>
+                    <a href="#" class="footer_link">somelink</a>
+                </li>
+            </ul>
+            <ul class="footer__column">
+                <li class="footer__item">
+                    <a href="#" class="footer_link">somelink</a>
+                    <a href="#" class="footer_link">somelink</a>
+                    <a href="#" class="footer_link">somelink</a>
+                    <a href="#" class="footer_link">somelink</a>
+                    <a href="#" class="footer_link">somelink</a>
+                </li>
+            </ul>
+        </div>
+        <div class="footer__row">
+            <div class="footer__copyright copyright">
+                <p class="copyright__text">&copy; 2025 All rights reserved. The source code is available on GitHub.</p>
+            </div>
+        </div>
     </div>
 </footer>
