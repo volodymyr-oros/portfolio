3382877c73553dcdfb5dbb7bba4c16c8992192a2
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2025-02-21 21:29:37 +0200

    Add burger buton

--- includes/header.php
+++ includes/header.php
@@ -1,29 +1,36 @@
 <header class="header template__subgrid">
-    <div class="header__content template__width-content">
-        <div class="header__content2 flex flex-alg-cnt flex-jst-btw">
+    <div class="header__content template__width-content flex flex-jst-btw flex-alg-cnt">
+
             <div class="header__logo logo">
-                <a href="#" class="logo__link">My portfolio</a>
+                <a href="/" class="logo__link">
+                    <span class="logo__text sr-only">My portfolio</span>
+                    <img src="/assets//images/header/web-programming.png" alt="logo" class="logo__image" height="60px" width="60px">
+                </a>
             </div>
-            <nav class="navigation">
-                <ul class="menu flex">
+            <nav id="primary-navigation" class="navigation flex flex-alg-cnt" area-visible="false">
+                <ul class="menu">
                     <li class="menu__item">
                         <a href="#" class="menu__link">Menu-category-1</a>
                     </li>
+                    <li class="menu__item">
+                        <a href="#" class="menu__link">Menu-category-1</a>
+                    </li>
+                    <li class="menu__item account">
+                        <span class="account__menu-name">Custome account menu</span>
+                        <ul class="account__navigation">
+                            <li class="account__item">
+                                <a href="/customer/account/signup.php" class="account__link">Sign up</a>
+                            </li>
+                            <li class="account__item">
+                                <a href="/customer/account/login.php" class="account__link">Log in</a>
+                            </li>
+                        </ul>
+                    </li>
                 </ul>
             </nav>
-            <div class="account">
-                <div class="account__name menu-parent">
-                    <a href="/" class="account__link">My account</a>
-                    <ul class="accout__menu menu-child first-child">
-                        <li class="account__item">
-                            <a href="/customer/account/signup.php" class="account__link">Sign up</a>
-                        </li>
-                        <li class="account__item">
-                            <a href="/customer/account/login.php" class="account__link">Log in</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+            <button id="burger-button" class="header__burger-menu" aria-controls="primary-navigation" area-expand="false">
+                <span class="header__burger-line"></span>
+                <span class="sr-only">Burger menu</span>
+            </button>
     </div>
 </header>
