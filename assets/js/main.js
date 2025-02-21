8ae20a17785aabf94e6e0113f93e2974ff8479fa
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2025-02-21 21:32:47 +0200

    Update burger menu script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,30 @@
 const burgerButton = document.querySelector("#burger-button");
 const primaryNavigation = document.querySelector("#primary-navigation");
+const overlay = document.createElement('div');
+overlay.classList.add('menu__overlay');
 
 burgerButton.addEventListener('click', () => {
     if (primaryNavigation.getAttribute('area-visible') === 'false') {
-        primaryNavigation.setAttribute('area-visible', 'true');
-        burgerButton.setAttribute('area-expand', 'true');
+        openBurgerMenu();
     } else if (primaryNavigation.getAttribute('area-visible') === 'true') {
-        primaryNavigation.setAttribute('area-visible', 'false');
-        burgerButton.setAttribute('area-expand', 'false');
+        closeBurgerMenu();
     }
 });
+
+overlay.addEventListener('click', () => {
+    if (primaryNavigation.getAttribute('area-visible') === 'true') {
+        closeBurgerMenu();
+    }
+});
+
+function openBurgerMenu() {
+    primaryNavigation.setAttribute('area-visible', 'true');
+    primaryNavigation.parentNode.appendChild(overlay);
+    burgerButton.setAttribute('area-expand', 'true');
+}
+
+function closeBurgerMenu() {
+    primaryNavigation.setAttribute('area-visible', 'false');
+    overlay.remove();
+    burgerButton.setAttribute('area-expand', 'false');
+}
