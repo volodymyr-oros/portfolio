fcaa17775a580720a65eafdec8ae7ae98a51763e
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2025-02-22 14:06:43 +0200

    Update page head HTML

--- includes/page_head.inc.php
+++ includes/page_head.inc.php
@@ -4,3 +4,5 @@
 <link rel="stylesheet" href="/assets/css/reset.css">
 <link rel="stylesheet" href="/assets/css/utility.css">
 <link rel="stylesheet" href="/assets/css/main.css">
+<link rel="icon" href="/assets/images/favicon.png" type="image/png">
+<script src="/assets/js/main.js" defer></script>
