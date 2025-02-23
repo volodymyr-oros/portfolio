ff90340f3fc541d71d32d84d8bed38f5b586f359
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2025-02-23 11:11:45 +0200

    Update form layoyt

--- customer/account/login.php
+++ customer/account/login.php
@@ -3,41 +3,39 @@
 
 <head>
     <?php require '../../includes/layout/page_head.inc.php'; ?>
-    <script src="/assets/js/form.js" defer></script>
     <title>Account login</title>
 </head>
 
 <body>
     <div class="wrapper template">
         <?php require '../../includes/layout/page_header.inc.php'; ?>
-        <main class="page">
-            <div class="page__box template--content">
-                <div class="page__content">
-                    <div class="page__title-wrapper">
-                        <h1 class="page__title-text">Login in to your account</h1>
+
+        <main class="page template__subgrid">
+            <div class="page__title template__width-content">
+                <h1 class="page__title-text">Account login</h1>
+            </div>
+            <?php require_once '../../includes/layout/page_message.inc.php'; ?>
+            <div class="page__login template__width-content">
+                <form action="../../includes/login/login.inc.php" method="POST" class="page__form form" autocomplete="on">
+
+                    <div class="form__row">
+                        <label for="email" class="form__label">Email</label>
+                        <input id="email" type="email" name="email" class="form__input" placeholder="example@mail.com" required autocomplete="email">
                     </div>
-                    <div class="page__section flex flex-jst-cnt">
-                        <form action="../../includes/login/login.inc.php" method="POST" class="form" autocomplete="on">
 
-                            <div class="form__row">
-                                <label for="email" class="form__label">Email</label>
-                                <input id="email" type="email" name="email" class="form__input" placeholder="Enter your email address..." required autocomplete="email">
-                            </div>
-
-                            <div class="form__row">
-                                <label for="password" class="form__label">Password</label>
-                                <input id="password" type="password" name="password" class="form__input" placeholder="Enter your password..." required autocomplete="current-password">
-                            </div>
-
-                            <div class="form__row form__row--action">
-                                <button class="form__button button button__submit" type="submit">Submit</button>
-                                <button id="form-reset" class="form__button button button__reset" type="reset" aria-label="Clear form fields">Reset</button>
-                            </div>
-                        </form>
+                    <div class="form__row">
+                        <label for="password" class="form__label">Password</label>
+                        <input id="password" type="password" name="password" class="form__input" placeholder="your password" required autocomplete="current-password">
                     </div>
-                </div>
+
+                    <div class="form__row form__row--action flex flex-jst-ard">
+                        <button id="form-reset" class="form__button button button--reset" type="reset" aria-label="Clear form fields">Reset</button>
+                        <button class="form__button button button--submit" type="submit">Submit</button>
+                    </div>
+                </form>
             </div>
         </main>
+
         <?php require '../../includes/layout/page_footer.inc.php'; ?>
     </div>
 </body>
--- customer/account/signup.php
+++ customer/account/signup.php
@@ -5,53 +5,53 @@ require_once '../../includes/signup/signup_view.inc.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <?php require '../../includes/layout/page_head.inc.php'; ?>
-    <script src="../../assets/js/form.js" defer></script>
     <title>Create an account</title>
 </head>
 
 <body>
     <div class="wrapper template">
         <?php require '../../includes/layout/page_header.inc.php'; ?>
+
         <?php check_signup_errors(); ?>
-        <main class="page">
-            <div class="page__box template__inherit template--content">
-                <div class="page__content">
-                    <div class="page__title-wrapper">
-                        <h1 class="page__title-text">Customer account create</h1>
+
+        <main class="page template__subgrid">
+            <div class="page__title template__width-content">
+                <h1 class="page__title-text">Create an account</h1>
+            </div>
+            <?php require_once '../../includes/layout/page_message.inc.php'; ?>
+            <div class="page__signup template__width-content">
+                <form action="../../includes/signup/signup.inc.php" method="POST" class="form" autocomplete="on">
+                    <div class="form__row">
+                        <label for="first-name" class="form__label">First name</label>
+                        <input id="first-name" type="text" name="first-name" class="form__input" placeholder="Enter your first name..." required autocomplete="given-name">
                     </div>
-                    <div class="page__section flex flex-jst-cnt">
-                        <form action="../../includes/signup/signup.inc.php" method="POST" class="form" autocomplete="on">
-                            <div class="form__row">
-                                <label for="first-name" class="form__label">First name</label>
-                                <input id="first-name" type="text" name="first-name" class="form__input" placeholder="Enter your first name..." required autocomplete="given-name">
-                            </div>
-                            <div class="form__row">
-                                <label for="last-name" class="form__label">Last name</label>
-                                <input id="last-name" type="text" name="last-name" class="form__input" placeholder="Enter your last name..." required autocomplete="family-name">
-                            </div>
-                            <div class="form__row">
-                                <label for="email" class="form__label">Email</label>
-                                <input id="email" type="email" name="email" class="form__input" placeholder="Enter your email address..." required autocomplete="email">
-                            </div>
-                            <div class="form__row">
-                                <label for="password" class="form__label">Password</label>
-                                <input id="password" type="password" name="password" class="form__input" placeholder="Enter your password..." required autocomplete="current-password">
-                            </div>
-                            <div class="form__row">
-                                <label for="confirm-password" class="form__label">Confirm password</label>
-                                <input id="confirm-password" type="password" name="confirm-password" class="form__input" placeholder="The same as your password..." required autocomplete="current-password">
-                            </div>
-                            <div class="form__row form__row--action">
-                                <button class="form__button button button__submit" type="submit">Submit</button>
-                                <button id="form-reset" class="form__button button button__reset" type="reset" aria-label="Clear form fields">Reset</button>
-                            </div>
-                        </form>
+                    <div class="form__row">
+                        <label for="last-name" class="form__label">Last name</label>
+                        <input id="last-name" type="text" name="last-name" class="form__input" placeholder="Enter your last name..." required autocomplete="family-name">
                     </div>
-                </div>
+                    <div class="form__row">
+                        <label for="email" class="form__label">Email</label>
+                        <input id="email" type="email" name="email" class="form__input" placeholder="Enter your email address..." required autocomplete="email">
+                    </div>
+                    <div class="form__row">
+                        <label for="password" class="form__label">Password</label>
+                        <input id="password" type="password" name="password" class="form__input" placeholder="Enter your password..." required autocomplete="current-password">
+                    </div>
+                    <div class="form__row">
+                        <label for="confirm-password" class="form__label">Confirm password</label>
+                        <input id="confirm-password" type="password" name="confirm-password" class="form__input" placeholder="The same as your password..." required autocomplete="current-password">
+                    </div>
+                    <div class="form__row form__row--action flex flex-jst-ard">
+                        <button id="form-reset" class="form__button button button--reset" type="reset" aria-label="Clear form fields">Reset</button>
+                        <button class="form__button button button--submit" type="submit">Submit</button>
+                    </div>
+                </form>
             </div>
         </main>
+
         <?php require '../../includes/layout/page_footer.inc.php'; ?>
     </div>
 </body>
