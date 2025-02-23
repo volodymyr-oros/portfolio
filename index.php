cd5b6ba1bcc1aa609fa49d217edc95bd14bdf8f5
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2025-02-23 11:12:15 +0200

    Update an index page layout

--- index.php
+++ index.php
@@ -9,10 +9,15 @@
 <body>
     <div class="wrapper template">
         <?php require_once './includes/layout/page_header.inc.php'; ?>
+
         <main class="page template__subgrid">
+            <div class="page__title template__width-content">
+                <h1 class="page__title-text sr-only">Home page</h1>
+            </div>
             <?php require_once './includes/layout/page_message.inc.php'; ?>
             <div class="page__content template__width-content"></div>
         </main>
+        
         <?php require_once './includes/layout/page_footer.inc.php'; ?>
     </div>
 </body>
