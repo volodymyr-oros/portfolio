45ae8e815a389e847a657e6681dd79efffb080d3
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2025-02-24 19:59:36 +0200

    Add logout sistem

---
+++ includes/logout.inc.php
@@ -0,0 +1,12 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    session_start();
+    session_unset();
+    session_destroy();
+
+    header('Location: ./index.php');
+    die();
+    
+}
