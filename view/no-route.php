5aa23e0dface7c85b427e131c4fb5083f6965f7a
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2025-03-05 21:41:49 +0200

    Update require statment paths

--- view/no-route.php
+++ view/no-route.php
@@ -3,15 +3,15 @@
 
 <head>
     <?php 
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/layout/page_head.inc.php'; 
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/signup/signup_view.inc.php';
+    require_once INCLUDES_PATH . '/layout/page_head.inc.php'; 
+    require_once INCLUDES_PATH . '/signup/signup_view.inc.php';
     ?>
     <title>No route</title>
 </head>
 
 <body>
     <div class="wrapper template">
-        <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/layout/page_header.inc.php'; ?>
+        <?php require_once INCLUDES_PATH . '/layout/page_header.inc.php'; ?>
 
         <main class="page template__subgrid">
             <div class="page__title template__width-content">
@@ -20,7 +20,7 @@
             <div class="page__content template__width-content"></div>
         </main>
         
-        <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/layout/page_footer.inc.php'; ?>
+        <?php require_once INCLUDES_PATH . '/layout/page_footer.inc.php'; ?>
     </div>
 </body>
 
