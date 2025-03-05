92e70fe0f3cbdb5917f08d1615588fd40db64c0e
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2025-03-05 21:42:28 +0200

    Update routes.php file

--- routes.php
+++ routes.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    '/' => 'view/home.php',
-    '/customer/login' => 'view/customer/login.php',
-    '/customer/signup' => 'view/customer/signup.php',
-    '/customer/my-account' => 'view/customer/account/my_account.php'
+    '/' => '../view/home.php',
+    '/customer/login' => '../view/customer/login.php',
+    '/customer/signup' => '../view/customer/signup.php',
+    '/customer/my-account' => '../view/customer/account/my_account.php'
 ];
