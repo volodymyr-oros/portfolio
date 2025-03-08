7635cd82e0e140d58dff141dd57a705d84923e36
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2025-03-08 13:49:47 +0200

    Add тхе technologies block template

--- view/home.php
+++ view/home.php
@@ -14,11 +14,11 @@
         <?php require_once INCLUDES_PATH . '/layout/page_header.inc.php'; ?>
 
         <main class="page template__subgrid">
-            <div class="page__title template__width-content">
+            <section class="page__title template__width-content">
                 <h1 class="page__title-text sr-only">Home page</h1>
-            </div>
+            </section>
             <?php check_signup_errors(); ?>
-            <div class="page__hero hero template__width-content">
+            <section class="page__hero hero template__width-content">
                 <div class="hero__frame">
                     <div class="hero__column">
                         <picture class="hero__picture">
@@ -33,8 +33,39 @@
                         <a href="" class="hero__link link">Find out more</a>
                     </div>
                 </div>
-            </div>
-            <div class="page__content template__width-content"></div>
+            </section>
+            <section class="page__technologies technologies template__width-content">
+                <div class="technologies__banner">
+                    <h2 class="technologies__title">Technologies Used</h2>
+                    <p class="technologies__description">This section showcases the programming languages that played a key role in building this website. Each card represents a language I used, providing insights into its purpose and significance in the project. Click on a card to explore more about how it was implemented.</p>
+                </div>
+                <div class="technologies__grid">
+                    <div class="technologies__cell">
+                        <div class="technologies__html">
+                            <h3 class="technologies__label">html</h3>
+                            <p class="technologies__text">Structures the content and elements of the website.</p>
+                        </div>
+                    </div>
+                    <div class="technologies__cell">
+                        <div class="technologies__css">
+                            <h3 class="technologies__label">css</h3>
+                            <p class="technologies__text">Styles the design, layout, and responsiveness.</p>
+                        </div>
+                    </div>
+                    <div class="technologies__cell">
+                        <div class="technologies__js">
+                            <h3 class="technologies__label">js</h3>
+                            <p class="technologies__text">Adds interactivity and enhances user experience.</p>
+                        </div>
+                    </div>
+                    <div class="technologies__cell">
+                        <div class="technologies__php">
+                            <h3 class="technologies__label">php</h3>
+                            <p class="technologies__text">Handles server-side logic and dynamic content.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
 
         <?php require_once INCLUDES_PATH . '/layout/page_footer.inc.php'; ?>
