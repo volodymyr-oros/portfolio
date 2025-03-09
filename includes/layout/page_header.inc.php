3324355855b8131753c06942852cc4587858ccf4
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2025-03-09 19:54:18 +0200

    Remove unused script after change menu logic

--- includes/layout/page_header.inc.php
+++ includes/layout/page_header.inc.php
@@ -3,17 +3,17 @@
             <div class="header__logo logo">
                 <a href="/" class="logo__link">
                     <span class="logo__text sr-only">My portfolio</span>
-                    <img src="/assets/images/header/web-programming.png" alt="logo" class="logo__image" height="60px" width="60px">
+                    <img src="/assets/images/header/web-programming.png" alt="logo" class="logo__image" width="100px">
                 </a>
             </div>
-            <nav id="primary-navigation" class="navigation flex flex-alg-cnt" area-visible="false">
+            <nav id="primary-navigation" class="navigation" area-visible="false">
                 <ul class="menu">
                     <li class="menu__item menu__item--first-lvl">
-                        <a href="#" class="menu__link">Menu-category-1</a>
+                        <a href="#" class="menu__link">Category</a>
                     </li>
-                    <li class="menu__item menu__item--first-lvl menu__item--nested" aria-controls="customer-account-menu2" area-expand="false">
-                        <span class="menu__link">Custome account menu</span>
-                        <ul id="customer-account-menu2" class="menu__sub-menu" area-visible="false">
+                    <li class="menu__item menu__item--first-lvl menu__item--nested">
+                        <span class="menu__link">Account</span>
+                        <ul class="menu__sub-menu">
                             <li class="menu__item menu__item--second-lvl">
                                 <a href="/customer/signup" class="menu__link">Sign up</a>
                             </li>
