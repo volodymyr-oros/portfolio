a82d14afc936a62ca5e23b3bafcb4851bc3dbeef
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2025-03-12 21:59:17 +0200

    Update home page layout

--- view/home.php
+++ view/home.php
@@ -14,67 +14,45 @@
         <?php require_once INCLUDES_PATH . '/layout/page_header.inc.php'; ?>
 
         <main class="page template__subgrid">
-            <section class="page__heading template__width-content">
-                <h1 class="page__heading-text sr-only">Home page</h1>
+            <section class="template__width-content">
+                <h1 class="sr-only">Home page</h1>
             </section>
             <?php check_signup_errors(); ?>
             <section class="page__hero hero template__width-content">
-                <div class="hero__wrapper">
-                    <div class="hero__column">
-                        <picture class="hero__picture frame">
-                            <source srcset="/assets/images/hero/red-panda_mobile.jpg" media="(max-width: 450px)" />
-                            <source srcset="/assets/images/hero/red-panda_tablet.jpg" media="(max-width: 800px)" />
-                            <img src="/assets/images/hero/red-panda_desktop.jpg" alt="MDN" />
-                        </picture>
+                <div class="hero__banner">
+                    <div class="hero__heading">
+                        <h2 class="hero__title">Banner title</h2>
                     </div>
-                    <div class="hero__column">
-                        <h2 class="hero__title">What is all about</h2>
-                        <p class="hero__description">This project showcases my skills, projects, and passion for web development. Here, you'll experience a blend of creativity, functionality, and attention to detail in every project.</p>
-                        <a href="" class="hero__link link">Find out more</a>
-                    </div>
+                    <picture class="hero__picture">
+                        <source srcset="/assets/images/hero/red-panda_mobile.jpg" media="(max-width: 450px)" />
+                        <source srcset="/assets/images/hero/red-panda_tablet.jpg" media="(max-width: 800px)" />
+                        <img class="hero__image" src="/assets/images/hero/red-panda_desktop.jpg" alt="MDN" />
+                    </picture>
                 </div>
             </section>
             <section class="page__technologies technologies template__width-content">
-                <div class="technologies__banner">
-                    <h2 class="technologies__title">Technologies Used</h2>
+                <div class="technologies__row technologies__row--head">
+                    <div class="technologies__heading">
+                        <h2 class="technologies__title">Technologies Used</h2>
+                    </div>
                     <p class="technologies__description">Here you can finde showcases the programming languages that played a key role in building this website. Each card represents a language I used, providing insights into its purpose and significance in the project. Click on a card to explore more about how it was implemented.</p>
                 </div>
-                <div class="technologies__grid">
-                    <a href="/" class="technologies__cell frame">
-                        <div class="technologies__item technologies__item--html">
-                            <div>
-                                <h3 class="technologies__label">html</h3>
-                                <p class="technologies__text">Structures the content and elements of the website.</p>
-                            </div>
-                            <img src="/assets/images/technologies/html.png" class="technologies__img" alt="html" height="200px">
-                        </div>
+                <div class="technologies__row technologies__row--grid">
+                    <a href="/" class="technologies__cell technologies__cell--html frame">
+                        <h3 class="technologies__label technologies__label--html">html</h3>
+                        <p class="technologies__description technologies__description--card">Structures the content and elements of the website.</p>
                     </a>
-                    <a href="/" class="technologies__cell frame">
-                        <div class="technologies__item technologies__item--html">
-                            <div>
-                                <h3 class="technologies__label">css</h3>
-                                <p class="technologies__text">Styles the design, layout, and responsiveness.</p>
-                            </div>
-                            <img src="/assets/images/technologies/css.png" class="technologies__img" alt="html" height="200px">
-                        </div>
+                    <a href="/" class="technologies__cell technologies__cell--css frame">
+                        <h3 class="technologies__label technologies__label--css">css</h3>
+                        <p class="technologies__description technologies__description--card">Styles the design, layout, and responsiveness.</p>
                     </a>
-                    <a href="/" class="technologies__cell frame">
-                        <div class="technologies__item technologies__item--html">
-                            <div>
-                                <h3 class="technologies__label">js</h3>
-                                <p class="technologies__text">Adds interactivity and enhances user experience.</p>
-                            </div>
-                            <img src="/assets/images/technologies/js.png" class="technologies__img" alt="html" height="200px">
-                        </div>
+                    <a href="/" class="technologies__cell technologies__cell--js frame">
+                        <h3 class="technologies__label technologies__label--js">js</h3>
+                        <p class="technologies__description technologies__description--card">Adds interactivity and enhances user experience.</p>
                     </a>
-                    <a href="/" class="technologies__cell frame">
-                        <div class="technologies__item technologies__item--html">
-                            <div>
-                                <h3 class="technologies__label">php</h3>
-                                <p class="technologies__text">Handles server-side logic and dynamic content.</p>
-                            </div>
-                            <img src="/assets/images/technologies/php.png" class="technologies__img" alt="html" height="200px">
-                        </div>
+                    <a href="/" class="technologies__cell technologies__cell--php frame">
+                        <h3 class="technologies__label technologies__label--php">php</h3>
+                        <p class="technologies__description technologies__description--card">Handles server-side logic and dynamic content.</p>
                     </a>
                 </div>
             </section>
