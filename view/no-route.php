7165d080f58d4cef7a96f0118b566a03b815b9d7
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2025-03-13 19:52:08 +0200

    Update no 404 page

--- view/no-route.php
+++ view/no-route.php
@@ -6,7 +6,6 @@
 <head>
     <?php 
     require_once INCLUDES_PATH . '/layout/page_head.inc.php'; 
-    require_once INCLUDES_PATH . '/signup/signup_view.inc.php';
     ?>
     <title>No route</title>
 </head>
@@ -16,10 +15,23 @@
         <?php require_once INCLUDES_PATH . '/layout/page_header.inc.php'; ?>
 
         <main class="page template__subgrid">
-            <div class="page__title template__width-content">
-                <h1 class="page__title-text">404 Not found</h1>
-            </div>
-            <div class="page__content template__width-content"></div>
+            <section class="page__heading template__width-content">
+                <h1 class="page__title">404 - Page Not Found</h1>
+            </section>
+            <section class="page__content template__width-content">
+                <p class="page__description">Oops! The page you're looking for doesn't exist. It might have been moved, deleted, or never existed in the first place.</p>
+                <ul class="page__list page__list--unordered list">
+                    <li class="list__item">
+                        <a href="#" class="list__link">Try checking the URL for errors.</a>
+                    </li>
+                    <li class="list__item">
+                        <a href="/" class="list__link">Go back to the homepage.</a>
+                    </li>
+                    <li class="list__item">
+                        <a href="#" class="list__link">Contact us if you need further assistance.</a>
+                    </li>
+                </ul>
+            </section>
         </main>
         
         <?php require_once INCLUDES_PATH . '/layout/page_footer.inc.php'; ?>
