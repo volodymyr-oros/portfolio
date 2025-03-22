255976cf5fb47468213754a081d0793a76c8af38
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2025-03-22 10:47:58 +0200

    Add logged in session validation

--- includes/my_account/my_account.inc.php
+++ includes/my_account/my_account.inc.php
@@ -1,2 +1,9 @@
 <?php
 
+require_once 'my_account_contr.inc.php';
+
+
+if(is_user_not_logged_in()) {
+    header('Location: /customer/login');
+    exit();
+}
--- includes/my_account/my_account_contr.inc.php
+++ includes/my_account/my_account_contr.inc.php
@@ -1,3 +1,12 @@
 <?php
 
 declare(strict_types=1);
+
+function is_user_not_logged_in()
+{
+    if (!isset($_SESSION['user_id'])) {
+        return true;
+    } else {
+        return false;
+    }
+}
