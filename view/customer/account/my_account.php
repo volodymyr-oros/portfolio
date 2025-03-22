db8fd3c3476661457ab5ce341f5ce152f09341f3
Volodymyr Oros <orosvolodymyr.work@gmail.com>
2025-03-22 13:01:58 +0200

    Update my account page layout

--- view/customer/account/my_account.php
+++ view/customer/account/my_account.php
@@ -40,6 +40,9 @@ include_once INCLUDES_PATH . '/my_account/my_account_view.inc.php';
                             <a href="#" class="account__link">Payment information</a>
                         </li>
                         <li class="account__item">
+                            <a href="#" class="account__link">Settings</a>
+                        </li>
+                        <li class="account__item">
                             <form action="/customer/logout/action" method="post" class="account__logout">
                                 <button type="submit" class="account__link account__link--logout">Logout</button>
                             </form>
@@ -48,56 +51,84 @@ include_once INCLUDES_PATH . '/my_account/my_account_view.inc.php';
                 </div>
                 <div class="account__column account__column--content">
                     <div class="account__info">
-                    <div class="account__heading">
+                        <div class="account__heading">
                             <h2 class="account__title">Account information</h2>
                         </div>
+                        <ul class="account__attribute attribute">
+                            <li class="attribute__list">
+                                <span class="attribute__label">Status: <a href="#" class="attribute__value">Enable</a></span>
+                            </li>
+                            <li class="attribute__list">
+                                <span class="attribute__label">Role: <a href="#" class="attribute__value">Author</a></span>
+                            </li>
+                            <li class="attribute__list">
+                                <span class="attribute__label">Account balance: <a href="#" class="attribute__value">100$</a></span>
+                            </li>
+                            <li class="attribute__list">
+                                <span class="attribute__label">Review rating: <a href="#" class="attribute__value">5 out of 5</a></span>
+                            </li>
+                            <li class="attribute__list">
+                                <span class="attribute__label">Articles: <a href="#" class="attribute__value">6</a></span>
+                            </li>
+                            <li class="attribute__list">
+                                <a href="#" class="attribute__value">View more</a>
+                            </li>
+                        </ul>
                     </div>
                     <div class="account__addresses">
                         <div class="account__heading">
                             <h2 class="account__title">Account addresses</h2>
                         </div>
-                    </div>
-                    <div class="account__address account__address--billing address">
-                        <div class="account__heading">
-                            <h3 class="address__title">Billing address</h3>
+                        <div class="account__address account__address--billing address">
+                            <div class="address__heading">
+                                <h3 class="address__title">Billing address</h3>
+                            </div>
+                            <dl class="address__list">
+                                <dt class="address__term">Email:</dt>
+                                <dd class="address__details">lordcommander@wall.nw</dd>
+                                <dt class="address__term">Full name:</dt>
+                                <dd class="address__details">Jogn Show</dd>
+                                <dt class="address__term">Street address:</dt>
+                                <dd class="address__details">North wall, gate 2</dd>
+                                <dt class="address__term">Region:</dt>
+                                <dd class="address__details">Winterfell</dd>
+                                <dt class="address__term">Country:</dt>
+                                <dd class="address__details">North</dd>
+                                <dt class="address__term">Postal code:</dt>
+                                <dd class="address__details">AB12 3CD</dd>
+                                <dt class="address__term">Phone number:</dt>
+                                <dd class="address__details">+44-##-####-####</dd>
+                            </dl>
+                            <div class="address__action">
+                                <a href="#" class="address__link">Change</a>
+                                <a href="#" class="address__link">Edit</a>
+                            </div>
                         </div>
-                        <dl class="address__list">
-                            <dt class="address__term">Email:</dt>
-                            <dd class="address__details">lordcommander@wall.nw</dd>
-                            <dt class="address__term">Full name:</dt>
-                            <dd class="address__details">Jogn Show</dd>
-                            <dt class="address__term">Street address:</dt>
-                            <dd class="address__details">North wall, gate 2</dd>
-                            <dt class="address__term">Region:</dt>
-                            <dd class="address__details">Winterfell</dd>
-                            <dt class="address__term">Country:</dt>
-                            <dd class="address__details">North</dd>
-                            <dt class="address__term">Postal code:</dt>
-                            <dd class="address__details">AB12 3CD</dd>
-                            <dt class="address__term">Phone number:</dt>
-                            <dd class="address__details">+44-##-####-####</dd>
-                        </dl>
-                    </div>
-                    <div class="account__address account__address--shipping">
-                        <div class="account__heading">
-                            <h3 class="address__title">Billing address</h3>
+                        <div class="account__address account__address--shipping address">
+                            <div class="address__heading">
+                                <h3 class="address__title">Shipping address</h3>
+                            </div>
+                            <dl class="address__list">
+                                <dt class="address__term">Email:</dt>
+                                <dd class="address__details">lordcommander@wall.nw</dd>
+                                <dt class="address__term">Full name:</dt>
+                                <dd class="address__details">Jogn Show</dd>
+                                <dt class="address__term">Street address:</dt>
+                                <dd class="address__details">North wall, gate 2</dd>
+                                <dt class="address__term">Region:</dt>
+                                <dd class="address__details">Winterfell</dd>
+                                <dt class="address__term">Country:</dt>
+                                <dd class="address__details">North</dd>
+                                <dt class="address__term">Postal code:</dt>
+                                <dd class="address__details">AB12 3CD</dd>
+                                <dt class="address__term">Phone number:</dt>
+                                <dd class="address__details">+44-##-####-####</dd>
+                            </dl>
+                            <div class="address__action">
+                                <a href="#" class="address__link">Change</a>
+                                <a href="#" class="address__link">Edit</a>
+                            </div>
                         </div>
-                        <dl class="address__list">
-                            <dt class="address__term">Email:</dt>
-                            <dd class="address__details">lordcommander@wall.nw</dd>
-                            <dt class="address__term">Full name:</dt>
-                            <dd class="address__details">Jogn Show</dd>
-                            <dt class="address__term">Street address:</dt>
-                            <dd class="address__details">North wall, gate 2</dd>
-                            <dt class="address__term">Region:</dt>
-                            <dd class="address__details">Winterfell</dd>
-                            <dt class="address__term">Country:</dt>
-                            <dd class="address__details">North</dd>
-                            <dt class="address__term">Postal code:</dt>
-                            <dd class="address__details">AB12 3CD</dd>
-                            <dt class="address__term">Phone number:</dt>
-                            <dd class="address__details">+44-##-####-####</dd>
-                        </dl>
                     </div>
                 </div>
             </section>
